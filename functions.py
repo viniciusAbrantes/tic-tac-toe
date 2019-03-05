54089e8e80c09cbf50edb3afe0c0482f0d6e63fd
viniciusAbrantes <vinicius_abrantes@live.com>
2019-03-05 11:56:08 -0300

    Commiting changes

--- functions.py
+++ functions.py
@@ -1,32 +1,46 @@
 def play(gameboard, player, row_choice, col_choice):
-    if(gameboard[row_choice][col_choice] == 0):
-        try:
+    try:
+        if(gameboard[row_choice][col_choice] == 0):
             gameboard[row_choice][col_choice] = player
             return True, gameboard
-        except IndexError:
-            print("Pls inform Something between 0 and 2\n")
-        except:
-            print("Something went wrong!\n")
-    else:
-        print("This spot is not available!\n")
+        else:
+            print("This spot is not available!\n")
+    except IndexError:
+        print("Please inform Something between 0 and 2\n")
+    except:
+        print("Something went wrong!\n")
+
     return False, gameboard
 
 def print_gameboard(gameboard):
-    for row in gameboard:
-        print(row)
+    print("   " + "  ".join([str(i) for i in range(len(gameboard))]))
+    for count, row in enumerate(gameboard):
+        print(count,row)
 
 def check_win(gameboard):
+    #Horizontal
     for row in gameboard:
         if(check_equal(row)):
-            print("Horizontal winner!")
+            print_gameboard(gameboard)
+            print("Player %s won horizontally!" % (row[0]))
             return True
 
+    #Vertical
+    for i in range(len(gameboard)):
+        vertical = []
+        for row in gameboard:
+            vertical.append(row[i])
+        if(check_equal(vertical)):
+            print_gameboard(gameboard)
+            print("Player %s won vertically!" % (vertical[0]))
+            return True
+
+    #Diagonal
+    #TODO
     return False
 
 def check_equal(list):
-    list = iter(list)
-    try:
-        first = next(list)
-    except StopIteration:
+    if list.count(list[0]) == len(list) and list[0] != 0:
         return True
-    return all(first != 0 and first == rest for rest in list)
+    else:
+        return False
--- game.py
+++ game.py
@@ -1,17 +1,22 @@
 from functions import *
 from itertools import cycle
+import os
 
-gameboard = [[0,0,0], [0,0,0], [0,0,0]]
-#game_size = int(input("Inform the gameboard size: "))
+game_size = int(input("Inform the gameboard size: "))
+gameboard = [[0 for i in range(game_size)] for i in range(game_size)]
 
 players = cycle([1, 2])
 game_over = False
 while(not game_over):
     current_player = next(players)
     valid_choice = False
+
     while(not valid_choice):
         print_gameboard(gameboard)
         row_choice = int(input("Inform the row you wanna play: "))
         col_choice = int(input("Inform the column you wanna play: "))
+
+        #clean terminal
+        os.system('cls' if os.name == 'nt' else 'clear')
         valid_choice, gameboard = play(gameboard, current_player, row_choice, col_choice)
         game_over = check_win(gameboard)
